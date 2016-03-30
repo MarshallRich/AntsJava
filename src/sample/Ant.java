204d9995c45bf1b3474a9b475479422688735694
MarshallRich <rlmarsh@g.clemson.edu>
2016-03-30 11:54:45 -0400

    "initial import with static ants"

---
+++ src/sample/Ant.java
@@ -0,0 +1,14 @@
+package sample;
+
+/**
+ * Created by MacLap on 3/30/16.
+ */
+public class Ant {
+    double x;
+    double y;
+
+    public Ant(double x, double y) {
+        this.y = y;
+        this.x = x;
+    }
+}
